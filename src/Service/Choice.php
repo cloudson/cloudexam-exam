22ef21ee6e2261cef5b942d765c2b0cfadace75f
Claudson Oliveira <claudsonweb@gmail.com>
2015-02-05 00:16:26 -0200

    Add method that finds choices by exam

--- src/Service/Choice.php
+++ src/Service/Choice.php
@@ -2,11 +2,35 @@
 
 namespace CloudExam\Exam\Service; 
 
+use CloudExam\Exam\Repository\Choice as ChoiceRepository;
+
 class Choice
 {
+    protected $repository; 
+
+    public function __construct(ChoiceRepository $repo)
+    {
+        $this->repository = $repo; 
+    }
+
     public function getChoicesByQuestion($questionId) 
     {
-        return [];
+        $choices = $this->repository->findByQuestionId($questionId);      
+        $transfers = []; 
+        foreach ($choices as $choice) {
+            $transfers[] = $this->asTransfer($choice);
+        }   
+
+        return $transfers;
     }
+    
+    public function asTransfer($choice)
+    {
+        $transfer = new ChoiceTransfer;
+        $transfer->setTitle($choice->getTitle());
+ 
+        return $transfer;
+    }
+
 }
 
