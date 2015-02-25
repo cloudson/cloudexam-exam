b2ad2157d89b2bb10835d6bd7451f12a1d3daab8
Claudson Oliveira <claudsonweb@gmail.com>
2015-02-25 20:01:48 -0300

    Add choices for a question example

--- docs/insert.sql
+++ docs/insert.sql
@@ -1,3 +1,7 @@
 INSERT INTO `exam`.`Exam`(`name`,`slug`,`description`)VALUES("ZCE", "zce", "Exam about php zend certified engineer");
 INSERT INTO `exam`.`Question`(`exam_id`,`title`, `slug`) VALUES (1, "Which function return all methods of an object?", "which-function-return-all-methods-of-an-object") ;
 INSERT INTO `exam`.`Question`(`exam_id`,`title`, `slug`) VALUES (1, "Which these words are not reserveds in php 5.5?", "which-these-words-are-not-reserveds-in-php55");
+insert into `Choice` (`question_id`, `title`) values (1, 'get_methods');
+insert into `Choice` (`question_id`, `title`) values (1, 'get_class');
+insert into `Choice` (`question_id`, `title`) values (1, 'get_operators');
+insert into `Choice` (`question_id`, `title`) values (1, 'get_func_names');
