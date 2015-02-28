20d9f5858889abaee0ba6c4eeeed8ae1ac7dc02b
Claudson Oliveira <claudsonweb@gmail.com>
2015-02-28 02:55:56 -0300

    Does Attempt table not use Apply

--- docs/database.sql
+++ docs/database.sql
@@ -1,116 +1,173 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE DATABASE  IF NOT EXISTS `exam` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `exam`;
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: exam
+-- ------------------------------------------------------
+-- Server version 5.5.41-0ubuntu0.14.04.1
 
-DROP SCHEMA IF EXISTS `exam` ;
-CREATE SCHEMA IF NOT EXISTS `exam` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `exam` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `exam`.`Exam`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `exam`.`Exam` ;
+--
+-- Table structure for table `Apply`
+--
 
-CREATE TABLE IF NOT EXISTS `exam`.`Exam` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `slug` VARCHAR(255) NOT NULL,
-  `description` TEXT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NULL,
+DROP TABLE IF EXISTS `Apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Apply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) NOT NULL,
+  `user` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `slug_UNIQUE` (`slug` ASC))
-ENGINE = InnoDB;
+  KEY `fk_Apply_1_idx` (`exam_id`),
+  CONSTRAINT `fk_Apply_1` FOREIGN KEY (`exam_id`) REFERENCES `Exam` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Apply`
+--
 
--- -----------------------------------------------------
--- Table `exam`.`Question`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `exam`.`Question` ;
+LOCK TABLES `Apply` WRITE;
+/*!40000 ALTER TABLE `Apply` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Apply` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `exam`.`Question` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `exam_id` INT NOT NULL,
-  `title` VARCHAR(255) NOT NULL,
-  `choice_id` INT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NULL,
-  `slug` VARCHAR(25) NULL,
+--
+-- Table structure for table `Attempt`
+--
+
+DROP TABLE IF EXISTS `Attempt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Attempt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `apply_id` int(11) DEFAULT NULL,
+  `choice_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Question_1_idx` (`exam_id` ASC),
-  CONSTRAINT `fk_Question_1`
-    FOREIGN KEY (`exam_id`)
-    REFERENCES `exam`.`Exam` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_Try_1_idx` (`apply_id`),
+  KEY `fk_Try_2_idx` (`choice_id`),
+  CONSTRAINT `fk_Try_1` FOREIGN KEY (`apply_id`) REFERENCES `Apply` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Try_2` FOREIGN KEY (`choice_id`) REFERENCES `Choice` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Attempt`
+--
 
--- -----------------------------------------------------
--- Table `exam`.`Choice`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `exam`.`Choice` ;
+LOCK TABLES `Attempt` WRITE;
+/*!40000 ALTER TABLE `Attempt` DISABLE KEYS */;
+INSERT INTO `Attempt` VALUES (2,NULL,1);
+/*!40000 ALTER TABLE `Attempt` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `exam`.`Choice` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `question_id` INT NOT NULL,
-  `title` VARCHAR(255) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NULL,
+--
+-- Table structure for table `Choice`
+--
+
+DROP TABLE IF EXISTS `Choice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Choice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Option_1_idx` (`question_id` ASC),
-  CONSTRAINT `fk_Option_1`
-    FOREIGN KEY (`question_id`)
-    REFERENCES `exam`.`Question` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_Option_1_idx` (`question_id`),
+  CONSTRAINT `fk_Option_1` FOREIGN KEY (`question_id`) REFERENCES `Question` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Choice`
+--
 
--- -----------------------------------------------------
--- Table `exam`.`Apply`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `exam`.`Apply` ;
+LOCK TABLES `Choice` WRITE;
+/*!40000 ALTER TABLE `Choice` DISABLE KEYS */;
+INSERT INTO `Choice` VALUES (1,1,'get_methods','2015-02-25 14:14:23',NULL),(2,1,'get_class','2015-02-25 14:15:39',NULL),(3,1,'get_operators','2015-02-25 14:15:39',NULL),(4,1,'get_func_names','2015-02-25 14:15:40',NULL);
+/*!40000 ALTER TABLE `Choice` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `exam`.`Apply` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `exam_id` INT NOT NULL,
-  `user` VARCHAR(45) NULL,
+--
+-- Table structure for table `Exam`
+--
+
+DROP TABLE IF EXISTS `Exam`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Exam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Apply_1_idx` (`exam_id` ASC),
-  CONSTRAINT `fk_Apply_1`
-    FOREIGN KEY (`exam_id`)
-    REFERENCES `exam`.`Exam` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE KEY `slug_UNIQUE` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Exam`
+--
 
--- -----------------------------------------------------
--- Table `exam`.`Attempt`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `exam`.`Attempt` ;
+LOCK TABLES `Exam` WRITE;
+/*!40000 ALTER TABLE `Exam` DISABLE KEYS */;
+INSERT INTO `Exam` VALUES (1,'ZCE','zce','Exam about php zend certified engineer','2015-02-25 13:48:09',NULL),(2,'LP1','lp1','Exam about linux centification level one','2015-02-26 00:42:21',NULL),(3,'LP2','lp2','Exam about linux centification level two','2015-02-26 00:42:22',NULL),(4,'Mysql db admin','mysql-db-admin','Exam about mysql','2015-02-26 00:42:22',NULL),(5,'Java','java','Exam about Java','2015-02-26 00:42:23',NULL);
+/*!40000 ALTER TABLE `Exam` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `exam`.`Attempt` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `apply_id` INT NOT NULL,
-  `choice_id` INT NOT NULL,
+--
+-- Table structure for table `Question`
+--
+
+DROP TABLE IF EXISTS `Question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `choice_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `slug` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Try_1_idx` (`apply_id` ASC),
-  INDEX `fk_Try_2_idx` (`choice_id` ASC),
-  CONSTRAINT `fk_Try_1`
-    FOREIGN KEY (`apply_id`)
-    REFERENCES `exam`.`Apply` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Try_2`
-    FOREIGN KEY (`choice_id`)
-    REFERENCES `exam`.`Choice` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_Question_1_idx` (`exam_id`),
+  CONSTRAINT `fk_Question_1` FOREIGN KEY (`exam_id`) REFERENCES `Exam` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Question`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `Question` WRITE;
+/*!40000 ALTER TABLE `Question` DISABLE KEYS */;
+INSERT INTO `Question` VALUES (1,1,'Which function return all methods of an object?',1,'2015-02-25 13:48:09',NULL,'which-function-return-all'),(2,1,'Which these words are not reserveds in php 5.5?',NULL,'2015-02-25 13:48:09',NULL,'which-these-words-are-not');
+/*!40000 ALTER TABLE `Question` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-28  2:47:45
